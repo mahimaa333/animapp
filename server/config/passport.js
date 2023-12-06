c00588acfc869ab10050f566173f676b16818f5b
mahimaa333 <60659758+mahimaa333@users.noreply.github.com>
2023-12-06 19:57:15 +0530

    Update passport.js

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,35 +1,3 @@
-
-// before subscription starts
-// const JwtStrategy = require('passport-jwt').Strategy;
-// const ExtractJwt = require('passport-jwt').ExtractJwt;
-
-// const db = require('./connections');
-// const config = require('../config/database');
-
-// module.exports = function(passport){
-// 	let opts = {};
-// 	opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-// 	opts.secretOrKey = config.secret;
-// 	passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-// 		let sql = 'SELECT * FROM user WHERE user_id=?';
-// 	  	let query = db.query(sql,[jwt_payload.sub],(err,result, fields) =>{
-// 			if (err) {
-// 					return done(err, false);
-// 			}
-// 			if (query) {
-// 					return done(null, query);
-// 			} else {
-// 					return done(null, false);
-// 			}
-// 	    });
-// 	}));
-// }
-// before ends
-
-
-// after starts
-
-
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 
